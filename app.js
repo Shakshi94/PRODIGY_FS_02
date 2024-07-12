0ed9d04c6a9ff3ddf5c61d4aa5bc81ba9b0f5e1d
Shakshi Vishwakarma <vishwakarmasakshi80@gmail.com>
2024-07-12 09:52:25 +0530

    inital commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const mongoose = require('mongoose');
+const ejs = require('ejs')
+const ejsMate = require('ejs-mate');
 
+main()
+    .then(console.log('database connection created successfully!'))
+    .catch((err) => console.log(err));
+
+async function main(){
+   await mongoose.connect('mongodb://127.0.0.1:27017/employeeManagmentSystem');
+    
+}
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
 
 
 app.listen(port,()=>{
