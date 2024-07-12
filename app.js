d7c367b3bf6795aae6ace254a6f3340e2e1aaf58
Shakshi Vishwakarma <vishwakarmasakshi80@gmail.com>
2024-07-12 18:56:45 +0530

    add total number of employees

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const ejs = require('ejs')
 const engine = require('ejs-mate');
 const path = require('path');
+const Employee = require('./models/employee')
 
 main()
     .then(console.log('database connection created successfully!'))
@@ -23,9 +24,18 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/dashboard',(req,res)=>{
-    res.render('./employeeDetails/dashboard.ejs');
-})
+app.get('/dashboard',async (req,res)=>{
+ try {
+    const count = await Employee.countDocuments();
+    res.render('./employeeDetails/dashboard.ejs',{ employeeCount: count });
+  } catch (error) {
+    console.error('Error counting documents:', error);
+    res.status(500).send('Internal Server Error');
+  }
+    
+});
+
+
 app.listen(port,()=>{
     console.log(`server is listening at port ${port}`);
 })
